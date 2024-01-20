6c7c46d17875e1f495f30b8501e330066e40cc9d
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2024-01-20 11:37:14 +0100

    Add script for dumping db schema;

---
+++ dump_db.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+# dump script to postgres user home dir
+sudo -u postgres pg_dump -sc -f '/var/lib/postgres/data/musicd
+ash.sql' musicdash
+
+sudo mv /var/lib/postgres/data/musicdash.sql $argv[1]
