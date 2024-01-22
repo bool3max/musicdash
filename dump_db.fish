2e6447f1df48376a8320f2e3845fad2a20fcd6e4
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2024-01-22 08:49:10 +0100

    fix db creation script

--- dump_db.fish
+++ dump_db.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/fish
 
 # dump script to postgres user home dir
-sudo -u postgres pg_dump -sc -f '/var/lib/postgres/data/musicd
-ash.sql' musicdash
+sudo -u postgres pg_dump -sc -f '/var/lib/postgres/data/musicdash.sql' musicdash
 
 sudo mv /var/lib/postgres/data/musicdash.sql $argv[1]
