18d10458873e02d3e84f0d2a4dcef5a1575be3f8
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2024-04-20 19:03:58 +0200

    add comment explanations to dump_db.fish

--- dump_db.fish
+++ dump_db.fish
@@ -1,6 +1,13 @@
 #!/usr/bin/fish
 
-# dump script to postgres user home dir
+# generate SQL script with pg_dump as postgres linux user to home dir
+#   -s : only dump schemas and not the data that the tables contain
+#   -c : Output commands to DROP all the dumped database objects prior to
+#        outputting the commands for creating them. This is useful because I am
+#        constantly developing on two separate machines and SQL commands to create
+#        schemas fail if they already exist.
+
 sudo -u postgres pg_dump -sc -f '/var/lib/postgres/data/musicdash.sql' musicdash
 
+# move temp file from postgres user home dir to specified dir
 sudo mv /var/lib/postgres/data/musicdash.sql $argv[1]
