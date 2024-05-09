a485ac9730605c06b6fd49cf1b0c603f833535d7
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2024-05-09 21:11:54 +0200

    Finished most basic aggregator

--- webapi/aggregator.go
+++ webapi/aggregator.go
@@ -5,7 +5,11 @@ import (
 	"context"
 	"log"
 	"time"
+
+	"github.com/jackc/pgx/v5"
 )
+
+const AGGREGATOR_SLEEP_TIME = 30 * 50 * time.Second
 
 // An Aggregator is an object that's used to periodically aggregate registered users'
 // track plays from Spotify and preserve them to the database
@@ -33,6 +37,7 @@ func (ag *Aggregator) Run() {
 		}
 
 		for _, user := range users {
+			log.Printf("aggregator: processing user {%v}={%v}\n", user.Username, user.Id.String())
 			// obtain spotify client
 			if err := user.GetSpotifyAuth(context.Background()); err != nil {
 				log.Printf("error obtaining spotify client for user: {%v}: %v\n", user.Id.String(), err)
@@ -44,7 +49,7 @@ func (ag *Aggregator) Run() {
 			row := ag.db.Pool().QueryRow(
 				context.Background(),
 				`
-					select refreshedat
+					select coalesce(refreshedat, '1900-01-01T00:00:01Z')
 					from auth.user
 					where id=$1
 				`,
@@ -52,8 +57,11 @@ func (ag *Aggregator) Run() {
 			)
 
 			if err := row.Scan(&refreshedAt); err != nil {
+				log.Printf("error getting last refresh time: %v\n", err)
 				continue
 			}
+
+			log.Printf("last refresh: %+v\n", refreshedAt)
 
 			countRequestTracks := 0
 
@@ -63,13 +71,94 @@ func (ag *Aggregator) Run() {
 				countRequestTracks = int(time.Since(refreshedAt).Seconds() / 30)
 			}
 
-			plays, err := user.Spotify.GetRecentlyPlayedTracks(countRequestTracks)
+			log.Printf("requesting %v tracks from spotify api...\n", countRequestTracks)
+
+			playsNew, err := user.Spotify.GetRecentlyPlayedTracks(countRequestTracks)
 			if err != nil {
 				log.Printf("error getting recently played tracks for user: {%v}: %v\n", user.Id.String(), err)
+				continue
+			}
+
+			// get most recent saved play from database
+			playsOld, err := user.GetRecentPlaysFromDB(1, user.Spotify)
+			if err != nil {
+				log.Printf("error getting recently played tracks from db for user: {%v}: %v\n", user.Id.String(), err)
+				continue
+			}
+
+			// special case - user has no recorded plays at all, save all plays for response
+			if len(playsOld) == 0 {
+				log.Println("user has no plays in db -> saving all plays from response...")
+				if err := user.SavePlays(playsNew); err != nil {
+					log.Printf("error saving new plays for user {%v}: %v\n", user.Id.String(), err)
+				}
+
+				// update user's refreshedat..
+				_, err = ag.db.Pool().Exec(
+					context.Background(),
+					`
+					update auth.user
+					set refreshedat=@refreshedAt
+					where userid=@userId	
+				`,
+					pgx.NamedArgs{
+						"refreshedAt": time.Now(),
+						"userId":      user.Id,
+					},
+				)
+
+				if err != nil {
+					log.Printf("error setting refreshed at for current user: %v\n", err)
+					continue
+				}
+
+				continue
+			}
+
+			// otherwise, determine which new plays are never than most recent in db
+			mostRecentDBPlay := playsOld[0]
+			log.Printf("most recent play in db at: %+v\n", mostRecentDBPlay.At)
+			var upperBoundIndex int
+			for idx, newPlay := range playsNew {
+				if newPlay.At.Before(mostRecentDBPlay.At) || newPlay.At.Equal(mostRecentDBPlay.At) {
+					upperBoundIndex = idx
+					break
+				}
+
+				// last play, still newer than most recent
+				if idx == len(playsNew)-1 {
+					upperBoundIndex = len(playsNew)
+				}
+			}
+
+			// save all recent plays from the response that are newer than the most
+			// recent play recorded in the database
+			log.Printf("saving total of {%v} new plays...\n", len(playsNew[:upperBoundIndex]))
+			if err := user.SavePlays(playsNew[:upperBoundIndex]); err != nil {
+				log.Printf("error saving new plays for user {%v}: %v\n", user.Id.String(), err)
+				continue
+			}
+
+			// update user's refreshedat..
+			_, err = ag.db.Pool().Exec(
+				context.Background(),
+				`
+					update auth.user
+					set refreshedat=@refreshedAt
+					where id=@userId	
+				`,
+				pgx.NamedArgs{
+					"refreshedAt": time.Now(),
+					"userId":      user.Id,
+				},
+			)
+
+			if err != nil {
+				log.Printf("error setting refreshed at for current user: %v\n", err)
+				continue
 			}
 		}
 
-		// Max. amount of time to sleep and not miss a single track play on Spotify
-		time.Sleep(30 * 50 * time.Second)
+		time.Sleep(5 * time.Minute)
 	}
 }
