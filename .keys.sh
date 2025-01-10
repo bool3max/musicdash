623270040898e92b555470680c379814aafa6f76
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2025-01-10 15:54:28 +0100

    remove creds

--- .keys.sh
+++
@@ -1,5 +0,0 @@
-# temporary, delete later
-export MUSICDASH_LASTFM_API_KEY=ef35e4cec24f12dbc76db67fda4b79fd
-export MUSICDASH_SPOTIFY_SECRET=c348c8a0e346469eb327d30f7eed70a4
-export MUSICDASH_SPOTIFY_CLIENT_ID=a060f62bd2174b63bec6fd55c4320178
-export MUSICDASH_DATABASE_URL="postgres://postgres:@localhost:5432/musicdash"
